31dbd1661bcb25a80db0a292621d89d035eee0ba
TuncaAlper <alpertunca@gmail.com>
2021-02-13 23:09:58 +0100

    Implemented selecting text from document body.

--- background.js
+++ background.js
@@ -1,20 +1,15 @@
-let color = "#3aa757";
-
 chrome.runtime.onInstalled.addListener(async () => {
-	chrome.storage.sync.set({ color });
-	console.log("Default background color set to %cgreen", `color: ${color}`);
-
-	// grant access to console.log in developer mode
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-		chrome.declarativeContent.onPageChanged.addRules([
-			{
-				conditions: [
-					new chrome.declarativeContent.PageStateMatcher({
-						pageUrl: { hostEquals: "developer.chrome.com" },
-					}),
-				],
-				actions: [new chrome.declarativeContent.ShowPageAction()],
-			},
-		]);
-	});
+  // grant access to console.log in developer mode
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "developer.chrome.com" },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
 });
--- popup.js
+++ popup.js
@@ -1,26 +1,22 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+let selectText = document.getElementById("selectText");
+selectText.style.backgroundColor = "blue";
+selectText.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-chrome.storage.sync.get("color", ({ color }) => {
-	changeColor.style.backgroundColor = color;
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: setSelectedText,
+    },
+    (injResult) => {
+      document.getElementById("selectedTextContent").textContent =
+        injResult[0].result;
+    }
+  );
 });
-console.log("chrome", chrome.storage);
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+const setSelectedText = () => {
+  const selectedText = window.getSelection().toString();
 
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: setPageBackgroundColor,
-	});
-});
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-	// document.addEventListener
-	chrome.storage.sync.get("color", ({ color }) => {
-		document.body.style.backgroundColor = color;
-	});
-}
+  return selectedText;
+};
