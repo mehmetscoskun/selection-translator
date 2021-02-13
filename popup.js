98735e85d295dc4dc2b43d1f61c57836ecc2ec63
TuncaAlper <alpertunca@gmail.com>
2021-02-13 23:09:58 +0100

    Implemented selecting text from document body.

---
+++ popup.js
@@ -0,0 +1,22 @@
+let selectText = document.getElementById("selectText");
+selectText.style.backgroundColor = "blue";
+selectText.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: setSelectedText,
+    },
+    (injResult) => {
+      document.getElementById("selectedTextContent").textContent =
+        injResult[0].result;
+    }
+  );
+});
+
+const setSelectedText = () => {
+  const selectedText = window.getSelection().toString();
+
+  return selectedText;
+};
